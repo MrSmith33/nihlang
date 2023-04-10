27a7c990d45b9f36e22afa1ec3f850d5bcb0f2b9
Andrey Penechko <mrsmith33@yandex.ru>
2023-04-10 03:06:50 +0300

    Add more feature flags for memory sanitizer

--- source/vox/vm/feature_flags.d
+++ source/vox/vm/feature_flags.d
@@ -3,13 +3,16 @@
 /// Authors: Andrey Penechko
 module vox.vm.feature_flags;
 
-enum MEMORY_RELOCATIONS_PER_ALLOCATION = false;
+enum MEMORY_RELOCATIONS_PER_ALLOCATION = true;
 enum MEMORY_RELOCATIONS_PER_MEMORY = !MEMORY_RELOCATIONS_PER_ALLOCATION;
 
 // Checks for invariants that can be violated through an external API
 enum CONSISTENCY_CHECKS = true;
 // Detect reading from uninitialized memory
 enum SANITIZE_UNINITIALIZED_MEM = true;
-
+// Detect existance of pointers to heap or stack memory that was freed
+enum SANITIZE_DANGLING_PTR = true;
+// Detect when heap allocation loses last pointer and is not freed
+enum SANITIZE_MEM_LEAK = true;
 // Initializes all newly pushed registers
 enum INIT_REGISTERS = false;
